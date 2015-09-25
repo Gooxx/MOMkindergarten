01938b30d1f30f4c21cd211448da8712b7812c12
goooo <goooo@aaa.local>
2015-09-25 14:39:21 +0800

    init

---
+++ MOMKindergarten/MOMKindergarten/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MOMKindergarten
+//
+//  Created by goooo on 15/9/25.
+//  Copyright © 2015年 mom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MOMKindergarten/MOMKindergarten/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MOMKindergarten
+//
+//  Created by goooo on 15/9/25.
+//  Copyright © 2015年 mom. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
